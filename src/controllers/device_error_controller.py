e6f0c670f9eca51d2f4c482b40ecf126e2c25514
emil <e.pargevich@mail.ru>
2022-02-08 07:44:03 +0000

    changes

--- src/controllers/device_error_controller.py
+++ src/controllers/device_error_controller.py
@@ -1,13 +1,18 @@
 from src.services import device_error_service
+from src.middlewares import auth_middleware, role_middleware
 
 
 # GET DEVICE ERROR IDS
+@auth_middleware.check_authorize
+@role_middleware.check_role(["admin"])
 def get_device_error_ids():
     res = device_error_service.get_device_error_ids()
     return res
 
 
 # GET DEVICE ERROR BY ID
+@auth_middleware.check_authorize
+@role_middleware.check_role(["admin"])
 def get_device_error_by_id(device_error_id):
     res = device_error_service.get_device_error_by_id(device_error_id=device_error_id)
     return res
