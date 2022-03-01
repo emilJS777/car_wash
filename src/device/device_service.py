ec233b402af9bfe22228536f86541fb6afe9d381
emil <e.pargevich@mail.ru>
2022-03-01 12:42:24 +0000

    changes

--- src/device/device_service.py
+++ src/device/device_service.py
@@ -5,32 +5,32 @@ from typing import List
 
 
 # GET Device IDS
-async def get_device_ids():
-    device_ids: List[int] = await device_service_db.get_device_ids()
+def get_device_ids():
+    device_ids: List[int] = device_service_db.get_device_ids()
     return response(True, device_ids, 200)
 
 
 # GET DEVICE IDS BY OWNER ID
-async def get_device_ids_by_owner_id(owner_id: int):
-    device_ids: List[int] = await device_service_db.get_device_ids_by_owner(owner_id=owner_id)
+def get_device_ids_by_owner_id(owner_id: int):
+    device_ids: List[int] = device_service_db.get_device_ids_by_owner(owner_id=owner_id)
     return response(True, device_ids, 200)
 
 
 # CREATE DEVICE
-async def create_device(code: str, owner_id: int):
+def create_device(code: str, owner_id: int):
     # GET DEVICE BY CODE AND VERIFY. IF EXIST RETURN CONFLICT
-    if await device_service_db.get_device_by_code(code=code):
+    if device_service_db.get_device_by_code(code=code):
         return response(False, {'msg': 'device by this code exist'}, 409)
 
     # ELSE CREATE DEVICE AND RETURN OK
-    await device_service_db.create_device(code=code, owner_id=owner_id)
+    device_service_db.create_device(code=code, owner_id=owner_id)
     return response(True, {'msg': 'device successfully created!'}, 201)
 
 
 # GET DEVICE BY ID
-async def get_device_by_id(device_id: int):
+def get_device_by_id(device_id: int):
     # GET DEVICE BY ID AND VERIFY. IF NOT FOUND RETURN NOT FOUND
-    device: Device = await device_service_db.get_device_by_id(device_id=device_id)
+    device: Device = device_service_db.get_device_by_id(device_id=device_id)
     if not device:
         return response(False, {'msg': 'device not found'}, 404)
 
@@ -41,29 +41,29 @@ async def get_device_by_id(device_id: int):
 
 
 # UPDATE DEVICE
-async def update_device(device_id: int, code: str):
+def update_device(device_id: int, code: str):
     # GET DEVICE BY ID AND VERIFY. IF NOT FOUND RETURN NOT FOUND
-    if not await device_service_db.get_device_by_id(device_id=device_id):
+    if not device_service_db.get_device_by_id(device_id=device_id):
         return response(False, {'msg': 'device not found'}, 404)
 
     # GET DEVICE BY CODE AND VERIFY. IF EXIST RETURN CONFLICT
-    if await device_service_db.get_device_by_code(code=code):
+    if device_service_db.get_device_by_code(code=code):
         return response(False, {'msg': 'device by this code exist'}, 409)
 
     # ELSE UPDATE DEVICE AND RETURN OK
-    await device_service_db.update_device(device_id=device_id, code=code)
+    device_service_db.update_device(device_id=device_id, code=code)
     return response(True, {'msg': 'device successfully updated!'}, 200)
 
 
 # ***** DEVICE CONTEnT
 
 # UPDATE DEVICE CONTENT
-async def update_device_content(device_code: str, water: bool, lather: bool):
-    device: Device = await device_service_db.get_device_by_code(code=device_code)
+def update_device_content(device_code: str, water: bool, lather: bool):
+    device = device_service_db.get_device_by_code(code=device_code)
     if not device:
         return response(False, {'msg': 'device not found'}, 404)
 
-    device_content_updated: Device = await device_service_db.update_device_content(device_id=device.id, water=water, lather=lather)
+    device_content_updated = device_service_db.update_device_content(device_id=device.id, water=water, lather=lather)
     # RESPONSE OK AND DEVICE CONTENT FIELDS
     return response(True, {'device_id': device_content_updated.id,
                            'water': device_content_updated.water,
--- src/device/device_service_db.py
+++ src/device/device_service_db.py
@@ -5,64 +5,64 @@ from sqlalchemy import desc
 
 
 # GET Device IDS
-async def get_device_ids() -> List[int]:
+def get_device_ids() -> List[int]:
     ids: List[int] = []
-    devices: List[Device] = await Device.query.order_by(desc(Device.last_update)).all()
+    devices: List[Device] = Device.query.order_by(desc(Device.last_update)).all()
     for device in devices:
         ids.append(device.id)
     return ids
 
 
 # GET DEVICE IDS BY CAR WASH ID
-async def get_device_ids_by_owner(owner_id: int) -> List[int]:
+def get_device_ids_by_owner(owner_id: int) -> List[int]:
     ids: List[int] = []
-    devices: List[Device] = await Device.query.filter_by(owner_id=owner_id).order_by(desc(Device.last_update)).all()
+    devices: List[Device] = Device.query.filter_by(owner_id=owner_id).order_by(desc(Device.last_update)).all()
     for device in devices:
         ids.append(device.id)
     return ids
 
 
 # GET Device BY TITLE
-async def get_device_by_code(code: str) -> Device:
-    device: Device = await Device.query.filter_by(code=code).first()
+def get_device_by_code(code: str) -> Device:
+    device: Device = Device.query.filter_by(code=code).first()
     return device
 
 
 # GET Device BY ID
-async def get_device_by_id(device_id: int) -> Device:
-    device: Device = await Device.query.filter_by(id=device_id).first()
+def get_device_by_id(device_id: int) -> Device:
+    device: Device = Device.query.filter_by(id=device_id).first()
     return device
 
 
 # CREATE Device
-async def create_device(code: str, owner_id: int) -> Device:
+def create_device(code: str, owner_id: int) -> Device:
     device: Device = Device(code=code, owner_id=owner_id)
-    await device.save_db()
+    device.save_db()
     return device
 
 
 # UPDATE DEVICE
-async def update_device(device_id: int, code: str) -> Device:
-    device: Device = await Device.query.filter_by(id=device_id).first()
+def update_device(device_id: int, code: str) -> Device:
+    device: Device = Device.query.filter_by(id=device_id).first()
     device.code = code
     device.last_update = datetime.utcnow()
-    await device.update_db()
+    device.update_db()
     return device
 
 
 # ACTIVATE DEVICE
-async def activate_device(device_id: int) -> Device:
-    device: Device = await Device.query.filter_by(id=device_id).first()
+def activate_device(device_id: int) -> Device:
+    device: Device = Device.query.filter_by(id=device_id).first()
     device.active = True
-    await device.update_db()
+    device.update_db()
     return device
 
 
 # DEACTIVATE DEVICE
-async def deactivate_device(device_id: int) -> Device:
-    device: Device = await Device.query.filter_by(id=device_id).first()
+def deactivate_device(device_id: int) -> Device:
+    device: Device = Device.query.filter_by(id=device_id).first()
     device.active = False
-    await device.update_db()
+    device.update_db()
     return device
 
 
@@ -70,10 +70,10 @@ async def deactivate_device(device_id: int) -> Device:
 
 
 # UPDATE DEVICE CONTENT
-async def update_device_content(device_id: int, water: bool, lather: bool) -> Device:
-    device_content: Device = await Device.query.filter_by(id=device_id).first()
+def update_device_content(device_id: int, water: bool, lather: bool) -> Device:
+    device_content: Device = Device.query.filter_by(id=device_id).first()
     device_content.water = water
     device_content.lather = lather
     device_content.last_update = datetime.utcnow()
-    await device_content.update_db()
+    device_content.update_db()
     return device_content
