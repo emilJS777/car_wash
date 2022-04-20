e1328b811edce9129f68910dd7fd782cc2d7d55a
emilJS777 <40712762+emilJS777@users.noreply.github.com>
2022-04-20 21:41:38 +0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from src import app
 from src._general.utils import context_initializer
 
 
-context_initializer.Initializer()
+# context_initializer.Initializer()
 if __name__ == '__main__':
     # ON SERVER 5003
-    app.run(debug=True, port=5003)
+    app.run(debug=True, port=5002)
