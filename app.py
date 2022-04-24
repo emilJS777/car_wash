5043e91cf77df722448ca7c84bb28c921e3252fa
emilJS777 <40712762+emilJS777@users.noreply.github.com>
2022-04-24 14:08:32 +0400

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from src import app, msg_broker
 from src._general.utils import context_initializer
 import threading
 
-context_initializer.Initializer()
-threading.Thread(target=msg_broker.Broker).start()
+# context_initializer.Initializer()
+# threading.Thread(target=msg_broker.Broker).start()
 if __name__ == '__main__':
     # ON SERVER 5003
 
